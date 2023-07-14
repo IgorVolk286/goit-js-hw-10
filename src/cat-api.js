c40bc436f28e4ee1b820e45a04638f6ddf61a981
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-07-14 21:47:16 +0300

    1.3

--- src/cat-api.js
+++ src/cat-api.js
@@ -7,19 +7,8 @@ export function fetchBreeds() {
     'https://api.thecatapi.com/v1/breeds?api-key=live_nOr0ffgglv54os8QMllnWrZxGl7QAeBmahHlri8B5TzOK0ViN2gR9fQPv45iz86z'
   ).then(response => {
     if (!response.ok) {
-      throw Error(`bbhghgh`);
+      throw Error(`error`);
     }
     return response.json();
   });
 }
-
-// export function fetchCatByBreed(event) {
-//   const ids = event.currentTarget.value;
-//   console.log(ids);
-//   return fetch(
-//     `https://api.thecatapi.com/v1/images/search?breed_ids=${ids}&api-key=live_nOr0ffgglv54os8QMllnWrZxGl7QAeBmahHlri8B5TzOK0ViN2gR9fQPv45iz86z`
-//   ).then(respons => {
-//     return respons.json();
-//   });
-//   //   ids.reset();
-// }
--- src/index.js
+++ src/index.js
@@ -2,34 +2,37 @@
 // axios.defaults.headers.common['x-api-key'] =
 //   'live_nOr0ffgglv54os8QMllnWrZxGl7QAeBmahHlri8B5TzOK0ViN2gR9fQPv45iz86z';
 
+import { Report } from 'notiflix/build/notiflix-report-aio';
 import SlimSelect from 'slim-select';
 import { fetchBreeds } from './cat-api';
 // import { fetchCatByBreed } from './cat-api';
+const refs = {
+  input: document.querySelector('.breed-select'),
+  dive: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+};
+refs.input.hidden = true;
+refs.error.classList.add('visually-hidden');
 
-const input = document.querySelector('.breed-select');
-const dive = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-
-input.addEventListener('change', fetchCatByBreed);
+refs.input.addEventListener('change', fetchCatByBreed);
 
 fetchBreeds()
   .then(data => createInputOptions(data))
-
-  .catch(err => {
-    console.log(`Error`);
-  })
-  .finally();
+  .catch(err => Report.failure(refs.error.textContent))
+  .finally(() => refs.loader.classList.add('visually-hidden'));
 
 function createInputOptions(data) {
   data.map(({ reference_image_id, name }) => {
     const options = `<option value = ${reference_image_id} > ${name} </option> `;
 
-    input.insertAdjacentHTML('afterbegin', options);
+    refs.input.insertAdjacentHTML('afterbegin', options);
   });
+  refs.input.hidden = false;
 }
 
 function fetchCatByBreed(event) {
+  refs.loader.classList.remove('visually-hidden');
   const idCat = event.currentTarget.value;
   console.log(idCat);
   return fetch(
@@ -41,21 +44,24 @@ function fetchCatByBreed(event) {
 
     .then(data => {
       createCardCat(data);
-    });
+    })
+    .catch(error => Report.failure(refs.error.textContent))
+    .finally(() => refs.loader.classList.add('visually-hidden'));
 }
 
 function createCardCat(data) {
   const { url, breeds } = data;
 
-  const rt = breeds
+  let catMarck = breeds
     .map(
       ({ name, temperament, description }) =>
-        `<img src="${url}" alt="${name}">
-   <h2>${name}</h2>
-   <h3>${description}</h3>
-   <h3>${temperament}</h3>`
+        `<img  class= "catImg" src="${url}" alt="${name}">
+        <div class="info">
+   <h2 class="name-title"> ${name} </h2>
+   <h3 class="description">${description}</h3>
+   <h3 class="temperament">Temperament:${temperament}</h3></div>`
     )
     .join();
 
-  return (dive.innerHTML = rt);
+  return (refs.dive.innerHTML = catMarck);
 }
