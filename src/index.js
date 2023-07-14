2d478390d22bbbcaf2230e2092dce31818925d6a
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-07-14 08:11:59 +0300

    1.2

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ import { fetchBreeds } from './cat-api';
 
 const input = document.querySelector('.breed-select');
 const dive = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
 input.addEventListener('change', fetchCatByBreed);
 
 fetchBreeds()
   .then(data => createInputOptions(data))
+
   .catch(err => {
     console.log(`Error`);
   })
@@ -20,8 +24,6 @@ fetchBreeds()
 function createInputOptions(data) {
   data.map(({ reference_image_id, name }) => {
     const options = `<option value = ${reference_image_id} > ${name} </option> `;
-    // console.log(options);
-    // console.log(data);
 
     input.insertAdjacentHTML('afterbegin', options);
   });
@@ -42,27 +44,18 @@ function fetchCatByBreed(event) {
     });
 }
 
-// console.log(data.breeds);
-// console.log(data.url);
-// console.log(data);
-
 function createCardCat(data) {
   const { url, breeds } = data;
-  const rt = breeds
-    .map(el => {
-      `<img src="${url}" alt="">
-   <h2>${el.name}</h2>
-   <h3>${el.temperament}</h3>
-   <h3></h3>`;
-    })
-    .join();
-  dive.insertAdjacentHTML('beforeend', rt);
-}
 
-//   breeds.map(el => {
-//     console.log(el.description);
-//     `<img src="${url}" alt="">
-// <h2>${name}</h2>
-// <h3>${el.temperament}</h3>
-// <h3></h3>`;
-//   });
+  const rt = breeds
+    .map(
+      ({ name, temperament, description }) =>
+        `<img src="${url}" alt="${name}">
+   <h2>${name}</h2>
+   <h3>${description}</h3>
+   <h3>${temperament}</h3>`
+    )
+    .join();
+
+  return (dive.innerHTML = rt);
+}
